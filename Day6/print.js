c1531095e2f2a3531558b29bdec8b2d5394a2e53
JayanthPrathab <srjayanth2004@gmail.com>
2025-02-14 13:31:03 +0530

    Day6 commit1

---
+++ Day6/print.js
@@ -0,0 +1,20 @@
+// console.log("Hello World!!")
+// array=["a", 1,true]
+// console.log(array)
+// object1={
+//     "firstname":"jayanth", 
+//     "lastname":"prathab",
+//     "dept":"ece",
+//     "college":"SJIT"
+// }
+// console.log(object1);
+// console.log(object1.firstname);
+// console.log(object1['firstname']);
+// object2=new Object();
+// object2.firstname='edison'
+// object2.lastname='thomas'
+// object2.dept='eee'
+// console.log(object2);
+set = new Set();
+set.add('hello')
+console.log(set);
