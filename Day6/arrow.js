9e0e6fbeb4b5dcf5341c2f34082f23920941f876
JayanthPrathab <srjayanth2004@gmail.com>
2025-02-17 11:16:57 +0530

    Day6 last commit

---
+++ Day6/arrow.js
@@ -0,0 +1,10 @@
+//function declaration
+function print(){
+    console.log("This is a function call!!")
+}
+print()
+//arrowfunction
+var arrowfunction=() => {
+    console.log("This is an arrow funciton, it is used with React as function keyword can't be used.")
+}
+arrowfunction()
