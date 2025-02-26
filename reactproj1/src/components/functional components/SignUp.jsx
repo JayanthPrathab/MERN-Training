a30c829703e34c0b0fece160ddf20e5df5bdcb11
JayanthPrathab <srjayanth2004@gmail.com>
2025-02-26 13:44:59 +0530

    Usestates for connecting backend is added

--- reactproj1/src/components/functional components/SignUp.jsx
+++ reactproj1/src/components/functional components/SignUp.jsx
@@ -1,30 +1,52 @@
+import { useState } from "react";
+import axios from 'axios';
 import "../css/SignUp.css";
 import { Link } from "react-router-dom";
 // import LoGin from './loGin';
 function SignUp() {
+  const [firstName,setFN]=useState("");
+  const [lastName,setLN]=useState("");
+  const [email,setEmail]=useState("");
+  const [pass,setPass]=useState("");
+  const [mobile,setMob]=useState();
+  const handleSignup=(event)=>{
+    event.preventDefault();  
+    axios.post("http://localhost:3001/signup",{
+    firstName:firstName,
+    lastName:lastName,
+    email:email,
+    password:pass,
+    mobile:mobile,});
+  }
   return (
     <div>
       <div className="outerbox">
         <h2>SignUp</h2>
-        <form action="">
+        <form onSubmit={handleSignup}>
+          First Name
+          <br />
+          <input type="text" value={firstName} onChange={(e)=>{setFN(e.target.value)}} />
+          <br />
+          <br />
+          Last Name
+          <br />
+          <input type="text" value={lastName} onChange={(e)=>{setLN(e.target.value)}} />
+          <br />
+          <br />
           Email
           <br />
-          <input type="text" />
+          <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
           <br />
           <br />
           Mobile Number <br />
-          <input type="number" />
+          <input type="number" value={mobile} onChange={(e)=>{setMob(e.target.value)}}/>
           <br />
           <br />
           Password <br />
-          <input type="password" />
+          <input type="password" value={pass} onChange={(e)=>{setPass(e.target.value)}} />
           <br />
           <br />
-          Re-enter Password <br />
-          <input type="password" />
-          <br />
-          <br />
-          <button>Submit</button>
+          <button type='submit'>Submit</button>
         </form><br />
         <div>
           Already having an account? <Link to="/loGin">Login</Link>
