d0ecf6886334c77c0cb0b739f17089fc02094e2b
JayanthPrathab <srjayanth2004@gmail.com>
2025-02-27 13:22:34 +0530

    Connecting F.E and B.E Globally

--- Day12-B.E/index.js
+++ Day12-B.E/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors())
+app.use(cors(
+  {
+    origin: 'https://mern-training-srj.vercel.app/'
+  }
+))
 app.use(express.json())
 const PORT = 3001;
 
