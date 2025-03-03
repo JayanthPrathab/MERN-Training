2b7ba9847f0a743c0e2e6118a91e9b61bdc77320
JayanthPrathab <srjayanth2004@gmail.com>
2025-03-03 19:00:26 +0530

    F.E & B.E Connection

--- PortfolioBuilder-B.E/index.js
+++ PortfolioBuilder-B.E/index.js
@@ -1,78 +1,84 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const bcrypt= require('bcrypt');
-const cors= require('cors')
-const Signup= require('./Models/signupSchema');
+const mdb = require("mongoose");
+const bcrypt=require('bcrypt');
+const cors=require('cors');
+const Signup = require("./Models/signupSchema");
 dotenv.config();
 
 const app = express();
-const corsptions={
-  origin:'https://portfolio-builder-woad.vercel.app/',
-  methods:['GET','POST','PUT'],
-  credentials:true
-}
-app.use(cors(corsptions));
-app.use(express.json());
-const port = 3001;
-mongoose
-  .connect(process.env.MONGODB_URL)
+app.use(cors())
+app.use(express.json())
+const PORT = 3001;
+
+// console.log(process.env.MONGODB_URL);
+const MONGODB_URL=process.env.MONGODB_URL
+mdb
+  .connect(MONGODB_URL)
   .then(() => {
-    console.log("Connection successful");
+    console.log("Connection sucessfull");
   })
   .catch((err) => {
-    console.log("Connection falied", err);
+    console.log("Check your connection string", err);
   });
-  
-  app.get("/", (req, res) => {
+
+app.get("/", (req, res) => {
     console.log("first")
   res.send("<h1>BackEnd Server Active</h1>");
 });
 
-  app.post('/signup',async(req,res)=>{
-    try{
-        const {firstName,lastName,email,password,mobile}=req.body;
-        const hashedpassword= await bcrypt.hash(password,10);
-        const newSignup=new Signup({
-            firstName:firstName,
-            lastName:lastName,
-            email:email,
-            password:hashedpassword,
-            mobile:mobile
-        })
-        newSignup.save();
-        console.log("Signup Successful");
-        res.status(201).json({message:"Signup Successful", isSignup:true})
-    }
-    catch(err){
-        console.log(err);
-        res.status(201).json({message:"User already exists!!! Log IN !!!", isSignup:false})
-    }
-  })
+app.post("/signup", async(req, res) => {
+  try {
+    const {firstName,lastName,email,password,mobile} = req.body
+    const hashedpassword= await bcrypt.hash(password, 10);
+    const newSignup = new Signup({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedpassword,
+      mobile: mobile,
+    })
+    newSignup.save();
+    console.log("Signup successful");
+    res.status(201).json({ message: "Signup Successful", isSignup: true });
+  } catch (err) {
+    console.log(err);
+    res.status(201).json({ message: "Signup unsuccessful", isSignup: false });
+  }
+});
 
-  app.post('/login',async(req,res)=>{
-    try{
-      const {email,password}=req.body;
-      const existingUser=await Signup.findOne({email:email});
-      if(existingUser){
-        const isValidpass=await bcrypt.compare(password,existingUser.password)
-        console.log(isValidpass);
-        if(isValidpass){
-          res.status(201).json({message:"Login Successful", isLogin:true})
-        }
-        else{
-          res.status(201).json({message:"Invalid Password", isLogin:false})
-        }
+app.get('/getsignupdetails',(req,res)=>{
+  const signup = Signup.find();
+  res.send("signup details fetched");
+})
+
+app.post('/login',async(req,res)=>{
+  console.log("welcome to login");
+  try{
+    const {email,password}=req.body;
+    const existingUser=await Signup.findOne({email :email});
+    console.log(existingUser);
+    if(existingUser){
+      const isValidpass= await bcrypt.compare(password,existingUser.password);
+      console.log(isValidpass)
+      if(isValidpass){
+        console.log(req.body);
+        res.status(201).json({message:"Login successful", isLogin:true})
       }
       else{
-        res.status(201).json({message:"Login unsuccessful", isLogin:false})
+        res.status(201).json({message:"Invalid Password", isLogin:false});
       }
     }
-    catch(err){
-
+    else{
+      res.status(201).json({message:"User not found!! Signup !!", isLogin:false})
     }
-  })
+  }
+  catch(error){
+    console.log("User not found!!! Signup First!!!")
+    res.status(201).json({message:"Login error", isLogin:false})
+  }
+})
 
-  app.listen(port, () => {
-    console.log("Server started successfully");
-  });
+app.listen(PORT, () => {
+  console.log("Server started successfully");
+});
