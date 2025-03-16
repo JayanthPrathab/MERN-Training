993697919157fab0ff1818c707596acc146f5de8
JayanthPrathab <srjayanth2004@gmail.com>
2025-03-16 19:38:48 +0530

    Frontend

--- PortfolioBuilder/src/App.jsx
+++ PortfolioBuilder/src/App.jsx
@@ -13,7 +13,7 @@ import Template2 from "./components/templates/template2";
 import Template3raw from "./components/templates/Template3raw";
 import Template3 from "./components/templates/template3";
 import Template4raw from "./components/templates/Template4raw";
-import Template4 from "./components/templates/template4";
+import Template4 from "./components/templates/Template4";
 import "./components/css/navbar.css";
 import "./components/css/footer.css";
 import "./components/css/plans.css";
