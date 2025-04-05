67e06b2dd1cd5d473d2b81c27cff03c03cba9e0e
JayanthPrathab <srjayanth2004@gmail.com>
2025-04-05 10:33:57 +0530

    For Redeployment

--- srjportfolio/src/components/HardwareProj.jsx
+++ srjportfolio/src/components/HardwareProj.jsx
@@ -15,10 +15,10 @@ const HardwareProj = () => {
       description: "A smart assistant that automates waste disposal at right times to make a clean society",
     },
     {
-      image: "https://picsum.photos/500/500?grayscale",
+      image: "https://images.unsplash.com/photo-1523848309072-c199db53f137?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       link: "https://google.com/",
-      title: "Item 3",
-      description: "This is pretty cool, right?",
+      title: "CMSS",
+      description: "Coal Mining Safety System, an integrated IoT system to prevent and ensure safety of miners in coal mines",
     },
     {
       image: "https://picsum.photos/600/600?grayscale",
